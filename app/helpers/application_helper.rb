0f1cf3645ced16d706298a669eb035b20bc1e09b
Adam Dawkins <adam@dragondrop.uk>
2021-03-21 15:55:55 +0000

    💅 Styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # rubocop:disable Rails/OutputSafety
+  def nbsp(text)
+    raw text.gsub(/\s(?!.*\s)/, "&nbsp;")
+  end
+  # rubocop:enable Rails/OutputSafety
 end
